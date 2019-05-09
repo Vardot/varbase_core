28376e7fb4c5861aa48925782615d2b5f4b07c53
Mohammed Razem <moe@alrazem.com>
2019-05-09 11:15:42 +0300

    Always deny access to '/admin/config/development/update-helper/clear'

--- modules/varbase_update_helper/src/Routing/RouteSubscriber.php
+++ modules/varbase_update_helper/src/Routing/RouteSubscriber.php
@@ -17,6 +17,10 @@ class RouteSubscriber extends RouteSubscriberBase {
     if ($route = $collection->get('checklistapi.checklists.update_helper_checklist')) {
       $route->setDefault('_title', 'Varbase update instructions');
     }
-  }
 
+    // Always deny access to '/admin/config/development/update-helper/clear'.
+    if ($route = $collection->get('checklistapi.checklists.update_helper_checklist.clear')) {
+      $route->setRequirement('_access', 'FALSE');
+    }
+  }
 }
