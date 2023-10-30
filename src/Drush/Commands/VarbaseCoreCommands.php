bb24d108fb0d1282d444540e4fbd3033ff593945
Rajab Natshah <rajabn@gmail.com>
2023-10-30 16:46:15 +0300

    Issue #3397695: Add  Varbase Drush commands to address non-existent permissions and resolve any inconsistencies in entity and field definitions

---
+++ src/Drush/Commands/VarbaseCoreCommands.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\varbase_core\Drush\Commands;
+
+use Drush\Attributes as CLI;
+use Drush\Commands\DrushCommands;
+
+use Vardot\Installer\ModuleInstallerFactory;
+use Vardot\Entity\EntityDefinitionUpdateManager;
+
+/**
+ * A Drush command file for Varbase Core.
+ */
+final class VarbaseCoreCommands extends DrushCommands {
+
+  /**
+   * Constructs a VarbaseCoreCommands object.
+   */
+  public function __construct() {
+    parent::__construct();
+  }
+
+  /**
+   * Remove non-existent permissions, to be used for upgrades with missing static and dynamic permissions.
+   */
+  #[CLI\Command(name: 'varbase:remove-non-existent-permissions', aliases: ['rnep'])]
+  #[CLI\Usage(name: 'varbase:remove-non-existent-permissions', description: 'Remove non-existent permissions, to be used for upgrades with missing static and dynamic permissions')]
+  public function removeNoneExistentPermissions() {
+
+    // Remove non-existent permissions Testing uninstall hook.
+    ModuleInstallerFactory::removeNoneExistentPermissions();
+
+    $this->logger()->success(dt('Removed non-existent permissions.'));
+  }
+
+  /**
+   * Entity updates to clear up any mismatched entity and/or field definitions
+   * Fix changes were detected in the entity type and field definitions.
+   */
+  #[CLI\Command(name: 'varbase:entity-update', aliases: ['edupdb'])]
+  #[CLI\Usage(name: 'varbase:entity-update', description: 'Entity updates to clear up any mismatched entity and/or field definitions. Fix changes were detected in the entity type and field definitions.')]
+  public function applyUpdatesWithEntityDefinitionUpdateManager() {
+
+    // Entity updates to clear up any mismatched entity and/or field definitions
+    // And Fix changes were detected in the entity type and field definitions.
+    \Drupal::classResolver()
+      ->getInstanceFromDefinition(EntityDefinitionUpdateManager::class)
+      ->applyUpdates();
+
+    $this->logger()->success(dt('Applied Entity updates for mismatched entity and/or field definitions'));
+  }
+
+}
