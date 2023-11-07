c0b95cf2a6cb98f0d36d3dc4da7cea3cf1df83be
Rajab Natshah <rajabn@gmail.com>
2023-11-07 16:26:01 +0300

    Issue #3399834: Add Allow custom account name to Varbase general settings to work with Email Registration ~2.0.0

--- src/Form/VarbaseGeneralSettingsForm.php
+++ src/Form/VarbaseGeneralSettingsForm.php
@@ -34,9 +34,16 @@ class VarbaseGeneralSettingsForm extends ConfigFormBase {
     $config = $this->config('varbase_core.general_settings');
     $form['settings']['welcome_status'] = [
       '#type' => 'checkbox',
-      '#default_value' => $config->get('welcome_status'),
+      '#default_value' => $config->get('welcome_status') ?? 1,
       '#title' => $this->t('Allow site to show welcome message'),
       '#description' => $this->t('This option will allow to display Varbase\'s welcome message on the homepage by adding <code>?welcome</code> to the URL. This option is automatically disabled after closing the welcome message. Check this then navigate to <a href="@front_page_with_welcome">@front_page_with_welcome</a> to see the welcome message again.', ['@front_page_with_welcome' => $front_page_with_welcome]),
+    ];
+
+    $form['settings']['allow_custom_account_name'] = [
+      '#type' => 'checkbox',
+      '#default_value' => $config->get('allow_custom_account_name') ?? 1,
+      '#title' => $this->t('Allow custom account name'),
+      '#description' => $this->t('This option enables users to create custom usernames and provides the ability to change the names of existing user accounts.'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -49,6 +56,8 @@ class VarbaseGeneralSettingsForm extends ConfigFormBase {
 
     $config = $this->config('varbase_core.general_settings');
     $config->set('welcome_status', $form_state->getValue('welcome_status'));
+    $config->set('allow_custom_account_name', $form_state->getValue('allow_custom_account_name'));
+
     $config->save();
 
     parent::submitForm($form, $form_state);
