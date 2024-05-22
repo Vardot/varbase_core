4c6522f4d6761ed82da63f2c002ee1d3e9e131b8
Rajab Natshah <rajabn@gmail.com>
2024-05-22 19:50:08 +0300

    Issue #3448429: Add varbase:composer:cleanup:patches drush command to clean up composer.json from any merge request patchs

--- src/Drush/Commands/VarbaseCoreCommands.php
+++ src/Drush/Commands/VarbaseCoreCommands.php
@@ -73,64 +73,240 @@ final class VarbaseCoreCommands extends DrushCommands {
   }
 
   /**
-   * Update composer.json replace any merge_request patch to local patch.
+   * Detect any merge request patch and download it to the local patches folder with a timestamp date and update the root composer.json file to use the timestamped local patch file.
    */
-  #[CLI\Command(name: 'varbase:composer-clean-up', aliases: ['var-ccu'])]
-  #[CLI\Usage(name: 'varbase:composer-clean-up', description: 'Detect any merge request patch and download it to local and update composer.json file.')]
+  #[CLI\Command(name: 'varbase:composer:cleanup:patches', aliases: ['var-ccup'])]
+  #[CLI\Usage(name: 'varbase:composer:cleanup:patches', description: 'Detect any merge request patch and download it to the local patches folder with a timestamp date and update the root composer.json file to use the timestamped local patch file.')]
   public function mergeRequestPatchesCleanup() {
     $root_directory = $this->getConfig()->get('runtime.project');
-    $json = file_get_contents($root_directory . '/composer.json');
+    $root_composer_json = file_get_contents($root_directory . '/composer.json');
 
-    $composer = json_decode($json, true);
-    $pattern = '/Issue #(\d+)/';
-    foreach ($composer['extra']['patches'] as $key => &$value) {
-      foreach ($value as $k => $v) {
-        if (str_contains($v, 'merge_requests')) {
+    $not_found_merge_request_patches = FALSE;
+
+    $root_composer = json_decode($root_composer_json, TRUE);
+
+    foreach ($root_composer['extra']['patches'] as $package_name => &$package_patches) {
+      foreach ($package_patches as $package_patch_title => $package_patch_remote_link) {
+        if (str_contains($package_patch_remote_link, "/-/merge_requests/")) {
+          $not_found_merge_request_patches = TRUE;
+
           try {
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $v);
+            curl_setopt($ch, CURLOPT_URL, $package_patch_remote_link);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
             $patch = curl_exec($ch);
             curl_close($ch);
           }
           catch (\Exception $e) {
-            \Drupal::logger('Varbase')->info("Unable to retrieve patch $V");
+            \Drupal::logger('Varbase')->info("Unable to retrieve patch $package_patch_remote_link");
+            $this->output()->writeln(dt("Unable to retrieve patch $package_patch_remote_link"));
           }
 
-          preg_match($pattern, $k, $matches);
-
-          if (!empty($matches[1])) {
-            $issue_id = $matches[1];
-
-            $directory = $root_directory . "/patches/";
-            /** @var \Drupal\Core\File\FileSystemInterface $file_system */
-            $file_system = \Drupal::service('file_system');
-            $file_system->prepareDirectory($directory, FileSystemInterface:: CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
-
-            try {
-              $patch_file = fopen($root_directory . "/patches/$issue_id.patch", "w");
-              fwrite($patch_file, $patch);
-              fclose($patch_file);
-              $value[$k] = "./patches/$issue_id.patch";
-            }
-            catch (\Exception $e) {
-              \Drupal::logger('Varbase')->info("Unable to save patch file $issue_id.patch");
-            }
+          // Get the processed package name.
+          $processed_package_name = '';
+          if ($package_name == 'drupal/core') {
+            $processed_package_name = 'drupal-core--';
           }
           else {
-            \Drupal::logger('Varbase')->info("Unable to retrieve the issue ID from \"$k\" from $key module patches list.");
+            $processed_package_name = str_replace('drupal/', '', $package_name) . '--';
           }
+
+          // The date of fetching or cleaning MR patches to local patches.
+          $fetch_date = date('Y-m-d') . "--";
+
+          // Get the issue id from the title of the patch. If it was listed.
+          preg_match('/#(\d+)/', $package_patch_title, $issue_id_matches);
+          $issue_id = '';
+          if (isset($issue_id_matches[1])) {
+            $issue_id = (string) $issue_id_matches[1] . "--";
+          }
+          else {
+            \Drupal::logger('Varbase')->info("Unable to retrieve the issue ID from \"$package_patch_title\" from $package_name package patches list.");
+            $this->output()->writeln(dt("Unable to retrieve the issue ID from \"$package_patch_title\" from $package_name package patches list."));
+          }
+
+          // Get the merge request id.
+          $mr_id = "mr";
+          $package_mr_patch = explode("/-/merge_requests/", $package_patch_remote_link);
+          preg_match('/(\d+)/', $package_mr_patch[1], $mr_id_matches);
+          if (isset($mr_id_matches[0])) {
+            $mr_id = "mr-" . (string) $mr_id_matches[0];
+          }
+          else {
+            \Drupal::logger('Varbase')->info("Unable to retrieve the merge request ID from \"$package_patch_remote_link\" for $package_name package.");
+            $this->output()->writeln(dt("Unable to retrieve the merge request ID from \"$package_patch_remote_link\" for $package_name package."));
+          }
+
+          $patch_file_name = $processed_package_name . $fetch_date . $issue_id . $mr_id . ".patch";
+
+          $patches_directory = $root_directory . "/patches/";
+          /** @var \Drupal\Core\File\FileSystemInterface $file_system */
+          $file_system = \Drupal::service('file_system');
+          $file_system->prepareDirectory($patches_directory, FileSystemInterface:: CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+
+          try {
+            $full_patch_file_name = $patches_directory . "/" . $patch_file_name;
+
+            $patch_file = fopen($full_patch_file_name, "w");
+            fwrite($patch_file, $patch);
+            fclose($patch_file);
+            $package_patches[$package_patch_title] = "./patches/" . $patch_file_name;
+
+            \Drupal::logger('Varbase')->info("Processed the patch for \"$package_patch_title\" for the $package_name package. <br /> From: $package_patch_remote_link <br /> To: ./patches/$patch_file_name");
+            $this->output()->writeln(dt("Processed the patch for \"$package_patch_title\" for the $package_name package"));
+            $this->output()->writeln(dt("From: $package_patch_remote_link"));
+            $this->output()->writeln(dt("To: ./patches/$patch_file_name"));
+          }
+          catch (\Exception $e) {
+            \Drupal::logger('Varbase')->info("Unable to save patch file $patch_file_name");
+            $this->output()->writeln(dt("Unable to save patch file $patch_file_name"));
+          }
+
+          $this->output()->writeln(dt("--------------------------------------------"));
         }
       }
     }
 
-    try {
-      file_put_contents($root_directory . '/composer.json', json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    if ($not_found_merge_request_patches) {
+      try {
+        file_put_contents($root_directory . '/composer.json', json_encode($root_composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+      }
+      catch (\Exception $e) {
+        \Drupal::logger('Varbase')->info("Unable to save composer.json file");
+        $this->output()->writeln(dt("Unable to save composer.json file"));
+      }
+
+      $this->output()->writeln(dt("Competed the composer clean up of merge request patches in the root composer.json file."));
     }
-    catch (\Exception $e) {
-      \Drupal::logger('Varbase')->info("Unable to save composer.json file");
+    else {
+      $this->output()->writeln(dt("No merge request patches were found in the root composer.json file."));
     }
+
+  }
+
+  /**
+   * Detect any merge request patch and download it to the local patches folder with a timestamp date and update patches-file json file to use the timestamped local patch file.
+   */
+  #[CLI\Command(name: 'varbase:composer:cleanup:patches-file', aliases: ['var-ccupf'])]
+  #[CLI\Usage(name: 'varbase:composer:cleanup:patches-file', description: 'Detect any merge request patch and download it to the local patches folder with a timestamp date and update patches-file json file to use the timestamped local patch file.')]
+  public function mergeRequestPatchesFileCleanup() {
+    $root_directory = $this->getConfig()->get('runtime.project');
+
+    $root_composer_json = file_get_contents($root_directory . '/composer.json');
+
+    $root_composer = json_decode($root_composer_json, TRUE);
+
+    if (isset($root_composer['extra']['patches-file'])) {
+      $patches_file_json = file_get_contents($root_directory . '/' . $root_composer['extra']['patches-file']);
+
+      $not_found_merge_request_patches = FALSE;
+
+      $patches_file_composer = json_decode($patches_file_json, TRUE);
+
+      foreach ($patches_file_composer['patches'] as $package_name => &$package_patches) {
+        foreach ($package_patches as $package_patch_title => $package_patch_remote_link) {
+          if (str_contains($package_patch_remote_link, "/-/merge_requests/")) {
+            $not_found_merge_request_patches = TRUE;
+
+            try {
+              $ch = curl_init();
+              curl_setopt($ch, CURLOPT_URL, $package_patch_remote_link);
+              curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+              curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+              $patch = curl_exec($ch);
+              curl_close($ch);
+            }
+            catch (\Exception $e) {
+              \Drupal::logger('Varbase')->info("Unable to retrieve patch $package_patch_remote_link");
+              $this->output()->writeln(dt("Unable to retrieve patch $package_patch_remote_link"));
+            }
+
+            // Get the processed package name.
+            $processed_package_name = '';
+            if ($package_name == 'drupal/core') {
+              $processed_package_name = 'drupal-core--';
+            }
+            else {
+              $processed_package_name = str_replace('drupal/', '', $package_name) . '--';
+            }
+
+            // The date of fetching or cleaning MR patches to local patches.
+            $fetch_date = date('Y-m-d') . "--";
+
+            // Get the issue id from the title of the patch. If it was listed.
+            preg_match('/#(\d+)/', $package_patch_title, $issue_id_matches);
+            $issue_id = '';
+            if (isset($issue_id_matches[1])) {
+              $issue_id = (string) $issue_id_matches[1] . "--";
+            }
+            else {
+              \Drupal::logger('Varbase')->info("Unable to retrieve the issue ID from \"$package_patch_title\" from $package_name package patches list.");
+              $this->output()->writeln(dt("Unable to retrieve the issue ID from \"$package_patch_title\" from $package_name package patches list."));
+            }
+
+            // Get the merge request id.
+            $mr_id = "mr";
+            $package_mr_patch = explode("/-/merge_requests/", $package_patch_remote_link);
+            preg_match('/(\d+)/', $package_mr_patch[1], $mr_id_matches);
+            if (isset($mr_id_matches[0])) {
+              $mr_id = "mr-" . (string) $mr_id_matches[0];
+            }
+            else {
+              \Drupal::logger('Varbase')->info("Unable to retrieve the merge request ID from \"$package_patch_remote_link\" for $package_name package.");
+              $this->output()->writeln(dt("Unable to retrieve the merge request ID from \"$package_patch_remote_link\" for $package_name package."));
+            }
+
+            $patch_file_name = $processed_package_name . $fetch_date . $issue_id . $mr_id . ".patch";
+
+            $patches_directory = $root_directory . "/patches/";
+            /** @var \Drupal\Core\File\FileSystemInterface $file_system */
+            $file_system = \Drupal::service('file_system');
+            $file_system->prepareDirectory($patches_directory, FileSystemInterface:: CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+
+            try {
+              $full_patch_file_name = $patches_directory . "/" . $patch_file_name;
+
+              $patch_file = fopen($full_patch_file_name, "w");
+              fwrite($patch_file, $patch);
+              fclose($patch_file);
+              $package_patches[$package_patch_title] = "./patches/" . $patch_file_name;
+
+              \Drupal::logger('Varbase')->info("Processed the patch for \"$package_patch_title\" for the $package_name package. <br /> From: $package_patch_remote_link <br /> To: ./patches/$patch_file_name");
+              $this->output()->writeln(dt("Processed the patch for \"$package_patch_title\" for the $package_name package"));
+              $this->output()->writeln(dt("From: $package_patch_remote_link"));
+              $this->output()->writeln(dt("To: ./patches/$patch_file_name"));
+            }
+            catch (\Exception $e) {
+              \Drupal::logger('Varbase')->info("Unable to save patch file $patch_file_name");
+              $this->output()->writeln(dt("Unable to save patch file $patch_file_name"));
+            }
+
+            $this->output()->writeln(dt("--------------------------------------------"));
+          }
+        }
+      }
+
+      if ($not_found_merge_request_patches) {
+        try {
+          file_put_contents($root_directory . '/' . $root_composer['extra']['patches-file'], json_encode($patches_file_composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        }
+        catch (\Exception $e) {
+          \Drupal::logger('Varbase')->info("Unable to save " . $root_composer['extra']['patches-file'] . " file");
+          $this->output()->writeln(dt("Unable to save " . $root_composer['extra']['patches-file'] . " file"));
+        }
+
+        $this->output()->writeln(dt("Competed the composer clean up of merge request patches in the " . $root_composer['extra']['patches-file'] . " file."));
+      }
+      else {
+        $this->output()->writeln(dt("No merge request patches were found in the " . $root_composer['extra']['patches-file'] . " file."));
+      }
+
+    }
+    else {
+      $this->output()->writeln(dt("No patches-file in the root composer.json file."));
+    }
+
   }
 
 }
