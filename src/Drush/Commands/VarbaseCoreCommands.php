9bf1b68ec3e8a946a0df11be4a1db7ef93bae3dc
Ahmad Khader <63112-Ahmad-Khader@users.noreply.drupalcode.org>
2024-06-10 17:38:02 +0300

    Issue #3447162 by Ahmad Khader: Fix Update aborted by: varbase_core_update_90006

--- src/Drush/Commands/VarbaseCoreCommands.php
+++ src/Drush/Commands/VarbaseCoreCommands.php
@@ -38,9 +38,9 @@ final class VarbaseCoreCommands extends DrushCommands {
       $this->logger()->success(dt('Removed non-existent permissions.'));
     }
     catch (\Exception $e) {
-      \Drupal::logger('Varbase')->critical('Error while drush varbase:remove-non-existent-permissions. !code !exception', [
-        '!code' => $e->getCode(),
-        '!exception' => $e->getMessage(),
+      \Drupal::logger('Varbase')->critical('Error while drush varbase:remove-non-existent-permissions. %code %exception', [
+        '%code' => $e->getCode(),
+        '%exception' => $e->getMessage(),
       ]);
     }
   }
@@ -65,9 +65,9 @@ final class VarbaseCoreCommands extends DrushCommands {
       $this->logger()->success(dt('Applied Entity updates for mismatched entity and/or field definitions'));
     }
     catch (\Exception $e) {
-      \Drupal::logger('Varbase')->critical('Error while drush varbase:entity-update. !code !exception', [
-        '!code' => $e->getCode(),
-        '!exception' => $e->getMessage(),
+      \Drupal::logger('Varbase')->critical('Error while drush varbase:entity-update. %code %exception', [
+        '%code' => $e->getCode(),
+        '%exception' => $e->getMessage(),
       ]);
     }
   }
